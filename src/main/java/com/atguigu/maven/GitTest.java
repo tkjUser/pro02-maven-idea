1b24f06c87a2694a2ea963eeaa2505835eb7e21d
tkj <tkj302994@163.com>
2022-03-31 17:16:57 +0800

    hot-fix第一次提交

--- src/main/java/com/atguigu/maven/GitTest.java
+++ src/main/java/com/atguigu/maven/GitTest.java
@@ -4,5 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("第三次提交的内容");
+        System.out.println("hot-fix分支上第一次提交的内容");
+
+
     }
 }
